f8c0d4e970f998f398eb2cfd5f60cb58a1630c3f
Lucas Domene <lucas.firmo@gmail.com>
2024-04-19 16:46:43 -0300

    feat: tailwind installation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-function App() {
-  return <div className="App">Hello World</div>;
+export default function App() {
+  return <div className="font-bold text-2xl">Hello World</div>;
 }
-
-export default App;
