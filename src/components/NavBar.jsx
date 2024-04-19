060729d913f9bd79fedd56ed7aa72f39660fe465
Lucas Domene <lucas.firmo@gmail.com>
2024-04-19 18:42:14 -0300

    feat: menu behavior

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/logo.png';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 export default function NavBar() {
   const menuItems = ['Home', 'About', 'Skills', 'Work', 'Contact'];
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  function handleMenuClick() {
+    setIsMenuOpened(!isMenuOpened);
+  }
 
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-primary text-gray-300">
@@ -20,18 +25,20 @@ export default function NavBar() {
       </ul>
 
       {/* Hamburguer */}
-      <div className="md:hidden z-10">
-        <FaBars />
+      <div className="md:hidden z-10 cursor-pointer" onClick={handleMenuClick}>
+        {isMenuOpened ? <FaTimes /> : <FaBars />}
       </div>
 
       {/* Mobile Menu */}
-      <ul className="md:hidden absolute top-0 left-0 w-full h-screen bg-primary flex flex-col justify-center items-center gap-14">
-        {menuItems.map((item) => (
-          <li className="text-4xl" key={item}>
-            {item}
-          </li>
-        ))}
-      </ul>
+      {isMenuOpened && (
+        <ul className="md:hidden absolute top-0 left-0 w-full h-screen bg-primary flex flex-col justify-center items-center gap-14">
+          {menuItems.map((item) => (
+            <li className="text-4xl cursor-pointer" key={item}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
 
       {/* Social Icons */}
     </div>
