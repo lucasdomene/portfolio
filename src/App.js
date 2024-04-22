f08f98f98cfc0cbfa59f57f5d80b824317a98124
Lucas Domene <lucas.firmo@gmail.com>
2024-04-22 17:45:49 -0300

    feat: Work component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from './components/About';
 import Home from './components/Home';
 import Skills from './components/Skills';
+import Work from './components/Work';
 import NavBar from './components/NavBar';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Home />
       <About />
       <Skills />
+      <Work />
     </div>
   );
 }
