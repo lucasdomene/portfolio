8c5490eb6e6ccbe34845591c6e98bb8acf570264
Lucas Domene <lucas.firmo@gmail.com>
2024-04-23 14:17:57 -0300

    feat: Work updates

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,6 +3,27 @@ import WorkImg from '../assets/projects/workImg.jpeg';
 import RealState from '../assets/projects/realestate.jpg';
 
 export default function Work() {
+  const projects = [
+    {
+      image: WorkImg,
+    },
+    {
+      image: RealState,
+    },
+    {
+      image: WorkImg,
+    },
+    {
+      image: RealState,
+    },
+    {
+      image: WorkImg,
+    },
+    {
+      image: RealState,
+    },
+  ];
+
   return (
     <div name="work" className="bg-primary w-full md:h-screen text-gray-300">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
@@ -13,30 +34,32 @@ export default function Work() {
           <p className="py-6">Check out some of my recent work</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-80">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://www.google.com">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://www.google.com">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {projects.map((project) => (
+            <WorkItem image={project.image} />
+          ))}
         </div>
+      </div>
+    </div>
+  );
+}
+
+function WorkItem({ image }) {
+  return (
+    <div className="relative shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center mx-auto">
+      <img src={image} alt="Work Item" className="rounded-md" />
+      <div className="absolute inset-0 opacity-0 group-hover:opacity-75 bg-purple-700" />
+      <div className="absolute inset-0 opacity-0 group-hover:opacity-100 flex justify-center items-center">
+        <a href="https://www.google.com">
+          <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+            Demo
+          </button>
+        </a>
+        <a href="https://www.google.com">
+          <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+            Code
+          </button>
+        </a>
       </div>
     </div>
   );
