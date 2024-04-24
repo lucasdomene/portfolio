1261a2f8b7f6f663aee3e40e5cb8e7664bdb83dd
Lucas Domene <lucas.firmo@gmail.com>
2024-04-24 20:24:33 -0300

    feat: change app title

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Skills from './components/Skills';
 import Work from './components/Work';
 import NavBar from './components/NavBar';
 import Contact from './components/Contact';
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    document.title = 'Lucas Domene';
+  });
+
   return (
     <div>
       <NavBar />
